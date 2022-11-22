465e47da8fb1011f845a931b4c29375d877c3853
tedtalktimmy <frtntn007@gmail.com>
2022-11-21 21:50:28 -0500

    fixed the stacked navbar from appearing on page reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
+      
       <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
       <h1 className="w-full text-3xl font-bold text-[#ffd906] m-4">REACT.</h1>
         <ul className='p-4 uppercase'>
@@ -30,6 +30,7 @@ const Navbar = () => {
           <li className="p-4 border-b border-gray-600">Merch</li>
           <li className="p-4">About</li>
         </ul>
+      </div>
       </div>
     </div>
   );
